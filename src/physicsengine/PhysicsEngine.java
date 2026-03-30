7455eb9ee37e0e5018bc335a9b6fd4999dba676b
Jordan Lee Brand <jordanleebrand@gmail.com>
2026-03-29 21:02:12 -0500

    Initial project setup

---
+++ src/physicsengine/PhysicsEngine.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package physicsengine;
+
+/**
+ *
+ * @author nook
+ */
+public class PhysicsEngine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
